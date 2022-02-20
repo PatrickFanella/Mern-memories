c9393180d5d706cfd308f542246382a692492767
Patrick Fanella <Fanella.Patrick@gmail.com>
2022-02-20 12:59:55 -0600

    users data is not being sent to database

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,51 +1,71 @@
 /** @format */
 
-import React, {useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import { AppBar, Typography, Toolbar, Button, Avatar } from '@material-ui/core';
-import useStyles from './styles';
-import memories from '../../images/memories.png';
+import React, { useState, useEffect } from 'react';
+import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
 
-function Navbar() {
+import memories from '../../images/memories.png';
+import * as actionType from '../../constants/actionTypes';
+import useStyles from './styles';
+
+const Navbar = () => {
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const navigate = useNavigate();
     const classes = useStyles();
 
-    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const logout = () => {
+        dispatch({ type: actionType.LOGOUT });
 
-    console.log(user)
+        navigate('/auth');
 
-    useEffect(()=>{
-        const token = user?.token
-        setUser(JSON.parse(localStorage.getItem('profile')))
-    },[])
+        setUser(null);
+    };
+
+    useEffect(() => {
+        const token = user?.token;
+
+        if (token) {
+            const decodedToken = decode(token);
+
+            if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+        }
+
+        setUser(JSON.parse(localStorage.getItem('profile')));
+    }, [location]);
+
     return (
         <AppBar className={classes.appBar} position='static' color='inherit'>
             <div className={classes.brandContainer}>
-                <Typography className={classes.heading} variant='h2' align='center' component={Link} to='/'>
+                <Typography component={Link} to='/' className={classes.heading} variant='h2' align='center'>
                     Memories
                 </Typography>
                 <img className={classes.image} src={memories} alt='icon' height='60' />
             </div>
             <Toolbar className={classes.toolbar}>
-                {user ? (
+                {user?.result ? (
                     <div className={classes.profile}>
-                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>
-                            {user.result.name.charAt(0)}
+                        <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>
+                            {user?.result.name.charAt(0)}
                         </Avatar>
                         <Typography className={classes.userName} variant='h6'>
-                            {user.result.name}
+                            {user?.result.name}
                         </Typography>
-                        <Button variant='contained' className={classes.logout} color='secondary' onClick={logoutHandler}>
+                        <Button variant='contained' className={classes.logout} color='secondary' onClick={logout}>
                             Logout
                         </Button>
                     </div>
                 ) : (
                     <Button component={Link} to='/auth' variant='contained' color='primary'>
-                        Sign in
+                        Sign In
                     </Button>
                 )}
             </Toolbar>
         </AppBar>
     );
-}
+};
 
 export default Navbar;
